997e9c348d38be68d8c37fac3af33668b745b358
Sakshimindhe <sakshimindhe22@hmail.com>
2023-04-19 23:44:02 -0700

    First commit

---
+++ daythree/daythree.js
@@ -0,0 +1,51 @@
+//single commet
+/*Multi-line Commet*/
+
+/*alert("Congrutulations!")
+alert("Hello");
+alert(4+2);
+let num=15;
+  alert(num) 
+
+  var num1=18;
+  alert(num1)
+  
+  const num2=7;
+  alert(num2);
+  num2=22;
+   alert(num2); 
+
+   var a=2 
+   var b=5
+   alert(a*=b);  */ 
+   
+   /*let a=13
+   //assignment operator
+       a*=5  
+       alert(a)
+
+    //relatonal operator
+       var res=12==0
+       alert(res)*/
+
+    //logical oprator
+     /*  var x=13;
+       var y=12;
+       alert(x==y && x>y)  
+
+       if(x>y)
+        alert("Sakshi")
+
+        else
+        alert("Mindhe")
+
+        typeof "22"
+         alert(typeof true) //boolean
+         alert(typeof "Sakshi")// string
+         alert(typeof 44) //number
+
+         let age=prompt("What's your age?",100)
+           alert(" Your age is" +age)*/
+
+
+           
---
+++ daythree/intro.js
@@ -0,0 +1,6 @@
+function display() 
+{
+    const x=document.getElementById("name").value;
+    var y=x.length;
+    document.getElementById("demo").innerHTML = "The length is: "+y;
+}
